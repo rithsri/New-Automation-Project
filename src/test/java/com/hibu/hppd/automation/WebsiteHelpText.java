121964dc8de1d8355303ac1e9739f9ef1382d454
YELLADWORKS\IB1429 <hemalatha.bondalapati@hibu.com>
2017-05-04 13:10:14 +0530

    added few more testNG Scripts

--- src/test/java/com/hibu/hppd/automation/WebsiteHelpText.java
+++ src/test/java/com/hibu/hppd/automation/WebsiteHelpText.java
@@ -30,21 +30,21 @@ public class WebsiteHelpText {
 			driver.manage().window().maximize();
 			driver.get("https://hibu--config03.cs15.my.salesforce.com/001e000000wzbsM"); //001e000000wzbsM  001e000000xDPza
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			  driver.findElement(By.id("username")).sendKeys("hemalatha.bondalapati@hibu.com.config03");
-			  driver.findElement(By.name("pw")).sendKeys("Hemalatha@123");
-			  WebElement login = driver.findElement(By.id("Login"));
-			  login.click(); 
-			  /*		driver.findElement(By.id("emc")).sendKeys("73364");
-				driver.findElement(By.id("save")).click();*/
-			  Thread.sleep(1000); 
-			  driver.findElement(By.name("customer_dashboard")).click();
-			  Thread.sleep(30000);
-			  ArrayList<String> newWindowHandle = new ArrayList<String> (driver.getWindowHandles());
-			  driver.switchTo().window(newWindowHandle.get(1));	    
-			  String title = driver.getTitle();
-			  System.out.println("switched to ----"+title);
-			  driver.findElement(By.xpath("//*[@id='overview-wrapper']/div/div[3]/div[1]/div/div[1]/div[1]/a/span")).click();
-			  Thread.sleep(4000);
+			driver.findElement(By.id("username")).sendKeys("hemalatha.bondalapati@hibu.com.config03");
+			driver.findElement(By.name("pw")).sendKeys("Hemalatha@123");
+			WebElement login = driver.findElement(By.id("Login"));
+			login.click(); 
+			driver.findElement(By.id("emc")).sendKeys("21229");
+			driver.findElement(By.id("save")).click();
+			Thread.sleep(1000); 
+			driver.findElement(By.name("customer_dashboard")).click();
+			Thread.sleep(30000);
+			ArrayList<String> newWindowHandle = new ArrayList<String> (driver.getWindowHandles());
+			driver.switchTo().window(newWindowHandle.get(1));	    
+			String title = driver.getTitle();
+			System.out.println("switched to ----"+title);
+			driver.findElement(By.xpath("//*[@id='overview-wrapper']/div/div[3]/div[1]/div/div[1]/div[1]/a/span")).click();
+			Thread.sleep(4000);
 	  }
 /*	  
 	  @BeforeMethod
@@ -138,6 +138,179 @@ public class WebsiteHelpText {
 	  FileUtils. copyFile(src1, new File("D:/Selenium/"+ System.currentTimeMillis() +"ReferringSites.png"));	  
 	  Assert.assertEquals("div", ele.getTagName());
   }
+  
+  @Test(priority=7)
+  public void kpiHelpText() throws InterruptedException, IOException{
+	  driver.findElement(By.cssSelector("div.row.section.summary > div.row.no-margin > div.col-xs-12.text-right.section-icon > .icon-button > img")).click();
+	  Thread.sleep(1000);
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"KPIHelp.png"));
+  }  
+  
+  @Test(priority=8)
+  public void crossProductUpliftHelpText() throws InterruptedException, IOException{
+	  driver.findElement(By.cssSelector("div.row.section.cross_product > div.row.no-margin > div:nth-Child(2) > .icon-button > img")).click();
+	  Thread.sleep(1000);
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"KPIHelp.png"));
+  }
+  
+  @Test(priority=9)
+  public void monthlyActivitySummaryHelpText() throws InterruptedException, IOException{
+	  driver.findElement(By.cssSelector("div.row.section.performance_monthly > div.row.no-margin > div:nth-Child(2) > button:nth-Child(2)")).click();
+	  Thread.sleep(1000);
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"KPIHelp.png"));
+  }
+  
+  @Test(priority=10)
+  public void websitePerformanceHelpText() throws InterruptedException, IOException{
+	  driver.findElement(By.cssSelector("div.row.section.overview > div.row.no-margin >  div:nth-Child(2) > button:nth-Child(2)")).click();
+	  Thread.sleep(1000);
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"KPIHelp.png"));
+  }
+  
+  @Test(priority=11)
+  public void mostVisitedHelpText() throws InterruptedException, IOException{
+	  driver.findElement(By.cssSelector("div.row.section.most_visited > div.row.no-margin >  div:nth-Child(2) > button:nth-Child(2)")).click();
+	  Thread.sleep(1000);
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"KPIHelp.png"));
+  }
+  
+  @Test(priority=12)
+  public void referringSitesHelpText() throws InterruptedException, IOException{
+	  driver.findElement(By.cssSelector("div.row.section.referring_sites > div.row.no-margin >  div:nth-Child(2) > button:nth-Child(2)")).click();
+	  Thread.sleep(1000);
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"KPIHelp.png"));
+  }
+  
+  @Test(priority=13)
+  public void monthlyActivitySummaryAllDevice() throws InterruptedException, IOException{
+	  WebElement ele=driver.findElement(By.cssSelector("div.row.section.performance_monthly"));
+	  ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", ele);
+	  Thread.sleep(1000);
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"MASAllDevice.png"));
+  }
+  
+  @Test(priority=14)
+  public void monthlyActivitySummaryWebDevice() throws InterruptedException, IOException{
+	  driver.findElement(By.cssSelector("div.row.section.performance_monthly > div:nth-Child(2) > div:nth-Child(2) > label:nth-Child(2)")).click();
+	  Thread.sleep(2000);
+	  WebElement ele=driver.findElement(By.cssSelector("div.row.section.performance_monthly"));
+	  ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", ele);	  
+	  Thread.sleep(1000);
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"MASWebDevice.png"));
+  }
+  
+  @Test(priority=15)
+  public void monthlyActivitySummaryMobileDevice() throws InterruptedException, IOException{
+	  driver.findElement(By.cssSelector("div.row.section.performance_monthly > div:nth-Child(2) > div:nth-Child(2) > label:nth-Child(3)")).click();
+	  Thread.sleep(2000);
+	  WebElement ele=driver.findElement(By.cssSelector("div.row.section.performance_monthly"));
+	  ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", ele);
+	  Thread.sleep(1000);;
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"MASMobileDevice.png"));
+  }
+  
+  @Test(priority=16)
+  public void websitePerformanceAllDevice() throws InterruptedException, IOException{
+	  WebElement ele=driver.findElement(By.cssSelector("div.row.section.overview"));
+	  ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", ele);
+	  Thread.sleep(1000);
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"WPAllDevice.png"));
+  }
+  
+  @Test(priority=17)
+  public void websitePerformanceWebDevice() throws InterruptedException, IOException{
+	  driver.findElement(By.cssSelector("div.row.section.overview > div:nth-Child(2) > div:nth-Child(2) > label:nth-Child(2)")).click();
+	  Thread.sleep(2000);
+	  WebElement ele=driver.findElement(By.cssSelector("div.row.section.overview"));
+	  ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", ele);	  
+	  Thread.sleep(1000);
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"WPWebDevice.png"));
+  }
+  
+  @Test(priority=18)
+  public void websitePerformanceMobileDevice() throws InterruptedException, IOException{
+	  driver.findElement(By.cssSelector("div.row.section.overview > div:nth-Child(2) > div:nth-Child(2) > label:nth-Child(3)")).click();
+	  Thread.sleep(2000);
+	  WebElement ele=driver.findElement(By.cssSelector("div.row.section.overview"));
+	  ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", ele);
+	  Thread.sleep(1000);;
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"WPMobileDevice.png"));
+  }
+  
+  @Test(priority=19)
+  public void mostVisitedAllDevice() throws InterruptedException, IOException{
+	  WebElement ele=driver.findElement(By.cssSelector("div.row.section.most_visited"));
+	  ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", ele);
+	  Thread.sleep(1000);
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"MVAllDevice.png"));
+  }
+  
+  @Test(priority=20)
+  public void mostVisitedWebDevice() throws InterruptedException, IOException{
+	  driver.findElement(By.cssSelector("div.row.section.most_visited > div:nth-Child(2) > div:nth-Child(2) > label:nth-Child(2)")).click();
+	  Thread.sleep(2000);
+	  WebElement ele=driver.findElement(By.cssSelector("div.row.section.most_visited"));
+	  ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", ele);	  
+	  Thread.sleep(1000);
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"MVWebDevice.png"));
+  }
+  
+  @Test(priority=21)
+  public void mostVisitedMobileDevice() throws InterruptedException, IOException{
+	  driver.findElement(By.cssSelector("div.row.section.most_visited > div:nth-Child(2) > div:nth-Child(2) > label:nth-Child(3)")).click();
+	  Thread.sleep(2000);
+	  WebElement ele=driver.findElement(By.cssSelector("div.row.section.most_visited"));
+	  ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", ele);
+	  Thread.sleep(1000);;
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"MVMobileDevice.png"));
+  }
+  
+  @Test(priority=22)
+  public void referringSitesAllDevice() throws InterruptedException, IOException{
+	  WebElement ele=driver.findElement(By.cssSelector("div.row.section.referring_sites"));
+	  ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", ele);
+	  Thread.sleep(1000);
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"RSAllDevice.png"));
+  }
+  
+  @Test(priority=23)
+  public void referringSitesWebDevice() throws InterruptedException, IOException{
+	  driver.findElement(By.cssSelector("div.row.section.referring_sites > div:nth-Child(2) > div:nth-Child(2) > label:nth-Child(2)")).click();
+	  Thread.sleep(2000);
+	  WebElement ele=driver.findElement(By.cssSelector("div.row.section.referring_sites"));
+	  ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", ele);	  
+	  Thread.sleep(1000);
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"RSWebDevice.png"));
+  }
+  
+  @Test(priority=24)
+  public void referringSitesMobileDevice() throws InterruptedException, IOException{
+	  driver.findElement(By.cssSelector("div.row.section.referring_sites > div:nth-Child(2) > div:nth-Child(2) > label:nth-Child(3)")).click();
+	  Thread.sleep(2000);
+	  WebElement ele=driver.findElement(By.cssSelector("div.row.section.referring_sites"));
+	  ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", ele);
+	  Thread.sleep(1000);;
+	  File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType.FILE);	
+	  FileUtils. copyFile(src, new File("D:/Selenium/"+System.currentTimeMillis()+"RSMobileDevice.png"));
+  }
+  
 	
   @Test(enabled=false)
   public void kpiText() throws InterruptedException, IOException{
